cfa2c44dd73b34656a39324d8c656d9eb5347b9e
Sam Lindstrom <lindstrom.s@gmail.com>
2017-05-01 08:36:27 -0600

    Redux experimentation

---
+++ src/client.js
@@ -0,0 +1,13 @@
+// import { combineReducers, createStore } from 'redux';
+
+// const userReducer = (state, actions) => {
+
+// };
+
+// const store = createStore(reducer, 0);
+
+// store.subscribe(() => {
+//   console.log("store changed", store.getState())
+// })
+
+// store.dispatch({type: "TEST", payload: 1})
